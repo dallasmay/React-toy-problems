7edcece65b98816e27f2bf452cd73313b0990574
dallasmay <dmmay9@gmail.com>
2022-07-29 10:09:43 -0600

    added logic for evenAndOdd, completed component

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 const EvenAndOdd = () => {
-  return <p>EvenAndOdd</p>;
-};
+  const [evenArray, setEvenArray] = useState([]);
+  const [oddArray, setOddArray] = useState([]);
+  const [userInput, setUserInput] = useState("");
 
+  const updateUserInput = (e) => {
+    setUserInput(e.target.value);
+  };
+
+  const evenOddSort = (input) => {
+    let evenNums = [];
+    let oddNums = [];
+    input = userInput;
+    let inputArr = input.split(",");
+    for (let num of inputArr) {
+      if (num % 2 === 0) {
+        evenNums.push(+num);
+      } else {
+        oddNums.push(+num);
+      }
+    }
+    setEvenArray(evenNums);
+    setOddArray(oddNums);
+    console.log(evenArray);
+    console.log(oddArray);
+    return
+  };
+
+  return (
+    <div className="puzzleBox evenAndOddPB">
+      <h4>Evens and Odds</h4>
+      <input className="inputLine" onChange={updateUserInput} />
+      <button className="confirmationButton" onClick={evenOddSort}>Split</button>
+      <span className="resultsBox">Evens: {JSON.stringify(evenArray)}</span>
+      <span className="resultsBox">Odds: {JSON.stringify(oddArray)}</span>
+    </div>
+  );
+};
 
 export default EvenAndOdd;
